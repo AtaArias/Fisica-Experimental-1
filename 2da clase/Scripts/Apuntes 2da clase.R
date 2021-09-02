34b5caeb15b8b4922ade3695c0f02378a04a8da0
AtaArias <atahulpq@gmail.com>
2021-09-02 14:41:05 -0300

    Create Apuntes 2da clase.R

---
+++ 2da clase/Scripts/Apuntes 2da clase.R
@@ -0,0 +1,31 @@
+# Cargar datos en R
+# data frames
+# tablas, cargar datos de entrada, las columadas / filas pueden tener diferetes
+# tipos de valores
+
+
+## Data sets ##
+# Saber como estás armado el data set, ás facil de graficar
+# Procesar facilmente los datos, + eficienia
+# Entendible para los terceros
+# Algoritmos estandar
+
+# Ordenar los datos toma mucho tiempo
+
+
+# Ejemplo cargando un archivo del cel #
+
+# En 00.tsv están los datos del celerómetro del cel
+# Para cargarlo en R hay que indicar cual es el directorio de trabajo
+
+print(getwd())
+
+#Campiar donde están mis documentos
+
+setwd("C:/Users/Atahulpq/Desktop/R/programas/Fisica-Experimental-1/2da clase")
+
+
+
+
+
+
