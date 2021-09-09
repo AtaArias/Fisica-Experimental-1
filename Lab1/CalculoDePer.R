bc76d7178e5590bc98dbb7564eac115ac4ecf0d1
AtaArias <atahulpq@gmail.com>
2021-09-09 18:11:44 -0300

    Lab1 experimental

---
+++ Lab1/CalculoDePer.R
@@ -0,0 +1,10 @@
+l <- seq(0.3, 2.25, 0.15)
+tao <- 2*pi*sqrt(l/9.8)
+tiempos <- tao * 200 / 60
+tiempo_total = sum(tiempos)
+tiempo_total
+nuestro_tiempo = sum(tiempos[seq(2, length(l), 2)])
+nuestro_tiempo
+
+iara = 2*pi*sqrt(2.55/9.8)
+iara *200 / 60
