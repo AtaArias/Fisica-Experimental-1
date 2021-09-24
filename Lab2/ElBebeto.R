b283428c172ec1851667d35aae51a094c53f6d8f
AtaArias <atahulpq@gmail.com>
2021-09-24 14:19:57 -0300

    Comentado del código, se agregó una columna para la media y otra para el el modulo de la aceleración medida

--- Lab2/ElBebeto.R
+++ Lab2/ElBebeto.R
@@ -9,22 +9,39 @@ files <- list.files(pattern = ".tsv")
 # tidy-data
 acel <- data.frame(NULL)
 for(file in files){
-  acel <- rbind(acel, read.csv(file = file, header = F, sep = "\t")[1:2000,])
+  acel <- rbind(acel, read.csv(file = file, header = F, sep = "\t",
+                colClasses = c("integer", "numeric", "numeric", "numeric","numeric", "integer" ))[1:2000,])
 }
 acel <- acel[,c(-1,-2)]
 colnames(acel) <- c("ax", "ay", "az", "name")
-horz <- acel[acel$name == 0,]
-mod0 <- sqrt(horz$ax**2+horz$ay**2+horz$az**2)
 
-for (i in 1:8)
+# calculamos el g de cada medida
+acel$g <- sqrt(acel$ax**2+acel$ay**2+acel$az**2)
+
+# creamos un lugar para la media
+acel$med <- NA
+
+# seleccionamos la primera medida, la horizontal
+horz <- acel[acel$name == 0,]
+for (i in 0:8)
 {
+  # seleccionamos los datos tita i
   current <- acel[acel$name == i,]
-  plot(sqrt(current$ax**2 + current$ay**2+ current$az**2), main = i)
   
-  modi <- sqrt(current$ax**2+ current$ay**2+ current$az**2)
+  media <- mean(current$g)
+  
+  acel[acel$name == i,]$med <- media
+  plot(current$g, main = i, ylab = "gravedad", xlab = "medida")
+  abline(h = media, col = "yellow", lwd = 3)
+  
+  # producto punto
   produc <- (horz$ax * current$ax)+(horz$ay * current$ay)+(horz$az * current$az)
-  coseno <- produc / (mod0 * modi)
+  
+  # angulo a través del arcoseno
+  coseno <- produc / (current$g * horz$g)
   ang <- acos(coseno) * 180 / pi
   plot(ang, main = paste(i, " ángulo"))
 }
 
+# este los coloco todos uno al lado del otro, para seguir trabajandolo
+plot(acel$g - acel$med)
