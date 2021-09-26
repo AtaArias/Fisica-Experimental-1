4f9fb7713811c6d60c9f75c99b40682b8b8a5274
AtaArias <atahulpq@gmail.com>
2021-09-26 01:37:25 -0300

    Primeros trabajos con los ángulos

--- Lab2/ElBebeto.R
+++ Lab2/ElBebeto.R
@@ -29,35 +29,55 @@ for(file in files){
   leyendo$g <- sqrt(leyendo$ax**2 +  leyendo$ay**2 + leyendo$az**2)
   
   mediana <- median(leyendo$g)
-  # 
-  # # filtramos los datos
-  # leyendo <- leyendo[leyendo$g < mediana + 0.1 & leyendo$g > mediana - 0.1,]
+
+  # filtramos los datos
+  leyendo <- leyendo[leyendo$g < mediana + 0.04 & leyendo$g > mediana - 0.04,]
   
   #  
-  # #los cortamos en 1973
-  # leyendo <- leyendo[1:2000,]
-  # 
+  #los cortamos en 1973
+  leyendo <- leyendo[1:2000,]
+
   
   # insertamos la media
   leyendo$media <- mean(leyendo$g)
   
-  print(length(leyendo[,1]))
-  
   acel <- rbind(acel, leyendo)
+  
+  paste(!!leyendo$name)
 }
 
+# Distribución de los datos de gravedad
+par(mar = c(6.1,5.1,5.1,3.1))
+plot(acel$g - acel$media,
+     main = "Distribución de las medidas de gravedad", xlab = "Número de medida", ylab = "Desviación de g (m/^2)",
+     cex.axis = 1.5, cex.lab = 1.5, cex.main = 3,
+     pch = 20, col = colores[acel$name + 1],
+     ylim = c(-0.06, 0.06))
+
+for (i in 0:8){
+  med = i * 10
+  legend(x = (i * 2000)-500, y = 0.06, 
+         legend = expr(paste(theta, " = ", !!med)), 
+         pch = 20, col = colores[i + 1], bty = "n")
+}
 
 # seleccionamos la primera medida, la horizontal
+angulos <- data.frame(NULL)
 horz <- acel[acel$name == 0,]
-for (i in 1:8)
-{
+for (i in 1:8){
   # seleccionamos los datos tita i
   current <- acel[acel$name == i,]
   
-  plot(current$g, main = i, ylab = "gravedad (m/s^2)", xlab = "número de medida",
-       pch = 20, col = colores[current$name + 1],
-       cex.main = 2, cex.axis = 1.5, cex.lab = 1.5)
-  abline(h = current$media, col = "yellow", lwd = 3)
+  # graficos el g para cada punto
+  plot(current$g, xlab = "Número de medida", ylab = "g medido (m/s^2)", 
+       main = expr( paste( "g de ", theta, " = ", !!(i * 10) )),
+       cex.main = 2, cex.lab = 1.5, cex.axis = 1.5,
+       pch = 20, col = colores[current$name[1] + 1])
+  # media de la gravedad
+  abline(h = current$media[1], col = "red", lwd = 2)
+  text(x = 200, y = current$media[1] + 0.0025, 
+       label = paste("g = ", round(current$media[1],4), "m/^2"), 
+       cex = 1.5, col = "red")
   
   # producto punto
   produc <- (horz$ax * current$ax)+(horz$ay * current$ay)+(horz$az * current$az)
@@ -65,11 +85,26 @@ for (i in 1:8)
   # angulo a través del arcoseno
   coseno <- produc / (current$g * horz$g)
   ang <- acos(coseno) * 180 / pi
+  SEM <- sd(ang) / sqrt(length(ang))
   plot(ang, main = paste(i * 10, " ángulo"), ylab = "ángulo (°)", xlab = "número de medida",
         pch = 20, col = colores[current$name + 1],
         cex.main = 2, cex.lab = 1.5, cex.axis = 1.5)
+  
+  # información sobre los angulos
+  datos <- c((i * 10), mean(ang), SEM)
+  angulos <- rbind(angulos, datos)
 }
+ 
+colnames(angulos) <- c("medida", "acelAng", "delta Acel")
 
-# este los coloca todos uno al lado del otro, para seguir trabajandolo
-plot(acel$g - acel$media, col = colores[acel$name + 1],
-     pch = 20)
+Trigs <- read.csv("trigDatos.csv")
+angulos$trigAng <- acos(Trigs$adyacente / Trigs$Hip) * (180 / pi)
+plot(y = angulos$acelAng, x = angulos$trigAng, pch = 20,
+     col = colores[angulos$medida / 10])
+abline(a = 0, b = 1, col = "red")
+arrows(x0 = angulos$medida, x1 = angulos$medida, 
+       y0 = angulos$acelAng + angulos$`delta Acel`, 
+       y1 = angulos$acelAng - angulos$`delta Acel`,
+       col = "yellow", length = 0)
+ajuste <- lm(angulos$acelAng ~ angulos$trigAng)
+abline(ajuste, lwd = 2, col = "yellow")
