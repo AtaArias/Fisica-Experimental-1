55c4d3d54328036e8bffab03c0c734d0477813f6
AtaArias <atahulpq@gmail.com>
2021-10-15 01:08:26 -0300

    Trabajado en el ajuste lineal

--- Lab3/Lab3Bonito.R
+++ Lab3/Lab3Bonito.R
@@ -1,4 +1,4 @@
-# setwd(paste(getwd(), "Lab3", "datos", sep = "/"))
+setwd(paste(getwd(), "Lab3", "datos", sep = "/"))
 #####
 # funciones
 gravedad <- function(set) {
@@ -22,8 +22,8 @@ angulos <- function(set1, set2){
 #####
 # constantes
 Raga <- 9.79316985
-n_hor <- "pisoLab3.tsv"
-n_inc <- "ang1lab3.tsv"
+n_hor <- "Jhorz.tsv"
+n_inc <- "aang.02.tsv"
 n_plano1 <- "MPlanoAta.04.csv"
 #####
 # lectura de datos
@@ -44,19 +44,31 @@ inc$g <- gravedad(inc)
 hor <- limpieza(hor) 
 inc <- limpieza(inc)
 #####
-
 angs <- angulos(hor, inc)
 hist(angs)
-# x(t) = 1/2 sen(alpha) * t²
-
+# x(t) = 1/2 sen(alpha) g * t²
+# y = 1/2 sen(alpha) g * x
 plot(p1$t, p1$x)
-
 m <- p1[p1$t < 2.5 & p1$t > 1.5,]
 plot(m$t, m$x)
 
-p2 <- p1[p1$t >1.8,]
-plot(p2$t, p2$x)
+p2 <- p1[p1$t >1.75,]
+t_0 <- p2$t[1]
+x_0 <- p2$x[1]
+p2$t <- p2$t - t_0
+p2$x <- p2$x - x_0
 
-hist(asin((2 * p1$x )/ (Raga * p1$t**2)))
-print(asin((2 * p1$x )/ (Raga * p1$t**2)))
-head(p1, 11)
+plot(p2$t, p2$x)
+plot(p2$t**2, p2$x)
+
+aju <- lm(p2$x ~ (p2$t**2))
+abline(aju)
+ord.origen <- aju$coefficients[1]
+pendiente <- aju$coefficients[2]
+lines(x = p2$x, y = ord.origen + pendiente * (p2$t**2))
+
+ang_per <- asin((2 * p2$x )/ (Raga * p2$t**2))
+hist(ang_per)
+print(mean(ang_per) * 180 / pi)
+# y = posición x = t²
+plot()
