d2033990ae302e013d71e1f7382644d8c311b9c2
AtaArias <atahulpq@gmail.com>
2021-11-05 13:34:33 -0300

    Datos de la cebra sin peso, casa

--- Lab7/Lab7Cod.R
+++ Lab7/Lab7Cod.R
@@ -9,6 +9,7 @@ limpieza <- function(dataSet){
   dataSet
 }
 
+lCasa <- read.csv("cebraSolaCasa.csv", header = T, sep = ";")
 liv <- read.csv("cebraSola.csv", header = T, sep = ";")
 liv2 <- read.csv("cebraSola2.csv", header = T, sep = ";")
 pes <- read.csv("cebraPesada.csv", header = T, sep = ";")
@@ -16,10 +17,15 @@ pes <- read.csv("cebraPesada.csv", header = T, sep = ";")
 pes <- limpieza(pes)
 liv <- limpieza(liv)
 liv2 <- limpieza(liv2)
+lCasa <- limpieza(Lcasa)
 mean(pes[pes$Interrupt == 12,]$t)
 for (i in pes$Tirada){
   pes[pes$Tirada == i,]$t <- pes[pes$Tirada == i,]$t - pes[pes$Tirada == i & pes$Interrupt == 1,]$t
 }
+for (i in lCasa$Tirada){
+  lCasa[lCasa$Tirada == i,]$t <- lCasa[lCasa$Tirada == i,]$t - lCasa[lCasa$Tirada == i & lCasa$Interrupt == 1,]$t
+}
 mean(pes[pes$Interrupt == 12,]$t)
+mean(lCasa[lCasa$Interrupt == 12,]$t)
 mean(liv[liv$Interrupt == 12,]$t)
 mean(liv2[liv2$Interrupt == 12,]$t)
