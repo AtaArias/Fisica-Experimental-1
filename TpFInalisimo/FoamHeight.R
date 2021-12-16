1c2cb91fc14642968c49a40d125c01e32b3f9a84
AtaArias <atahulpq@gmail.com>
2021-12-16 02:47:34 -0300

    Nuevo algoritmo para determinar la altura, se basa en el la segunda interrupción de arriba hacia abajo

--- TpFInalisimo/FoamHeight.R
+++ TpFInalisimo/FoamHeight.R
@@ -41,6 +41,8 @@ bots <- c()
 for (i in 1:8) {
   pic <- readPNG(files[i])
   pic <- as.matrix(pic[,,1])
+  org <- pic
+  
   pic <- (pic > 0.7)
   storage.mode(pic) <- "numeric"
   
@@ -49,21 +51,34 @@ for (i in 1:8) {
   h_bot <- c()
   for (i in 1:NCOL(pic)){
     columna <- pic[,i]
-    indices <- which(columna == 1)
-    if (length(indices) > 2){
-      h_top <- c(h_top, min(indices))
-      h_bot <- c(h_bot, max(indices))
+    columna <- columna == 1
+    list <- c(FALSE)
+    for (i in 2:length(columna)-1){
+        list <- c(list, (columna[i-1] != columna[i+1]) & columna[i])
+    }
+    indices <- which(list)
+    if (length(indices) > 1){
+      h_top <- c(h_top, tail(indices,2)[1])
+      h_bot <- c(h_bot, tail(indices,1))
     }
   }
   tops <- c(tops, mean(h_top))
   bots <- c(bots, median(h_bot))
   
-  pic[median(h_top):(median(h_top)+10),] <- 0.5
-  pic[median(h_bot):(median(h_bot)+10),] <- 0.5
+  # pic[median(h_top):(median(h_top)+10),] <- 0.5
+  # pic[median(h_bot):(median(h_bot)+10),] <- 0.5
+ 
+  org[median(h_top):(median(h_top)+10),] <- 0.5
+  org[median(h_bot):(median(h_bot)+10),] <- 0.5
 
   plot(1:2, type = "n")
-  rasterImage(pic, 1.2, 1.27, 1.8, 1.73)
+  rasterImage(org, 1.2, 1.27, 1.8, 1.73)
+  
+  # plot(1:2, type= "n")
+  # rasterImage(pic, 1.2, 1.27, 1.8, 1.73)
+  
 }
 bots
-tops
-plot(log10(seq(1, 240, 30)),bots)
+plot(tops)
+plot(log10(seq(1, 240, 30)),bots-tops)
+plot(tops)
