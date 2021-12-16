dd9c462b449d6a99ce8521b92c94d4764f6d6e27
AtaArias <atahulpq@gmail.com>
2021-12-16 09:35:20 -0300

    Se estaba filtrando mal, borramos los datos menores a la linea menor de la cerveza

--- TpFInalisimo/FoamHeight.R
+++ TpFInalisimo/FoamHeight.R
@@ -11,7 +11,7 @@ rasterImage(img.filtered, 1.2, 1.27, 1.8, 1.73)
 # hist(img.filtered)  
 # NCOL(img.filtered)
 
-img.sat <- (img.filtered > 0.85)
+img.sat <- (img.filtered < 0.9 & img.filtered > 0.7)
 storage.mode(img.sat) <-"numeric"
 
 # hist(img.sat)
@@ -62,30 +62,40 @@ for (i in 1:length(files)) {
   }
   
   # Que uso mean o median??
-  tops <- c(tops, median(tail(h_top,100)))
+  h_top <- h_top[h_top < median(h_bot)]
+  tops <- c(tops, median(h_top))
   bots <- c(bots, median(h_bot))
   
-  pic[tops[i]:(tops[i]+10),] <- 0.5
+  pic[tops[i]:(tops[i]+10),] <- 0.75
   pic[bots[i]:(bots[i]+10),] <- 0.5
   
-  org[tops[i]:(tops[i]+10),] <- 0.5
+  org[tops[i]:(tops[i]+10),] <- 0.75
   org[bots[i]:(bots[i]+10),] <- 0.5
 
   plot(1:2, type = "n")
-  rasterImage(org, 1.2, 1.27, 1.8, 1.73)
+  rasterImage(org, 1, 1, 2, 2)
   
   plot(1:2, type= "n")
-  rasterImage(pic, 1.2, 1.27, 1.8, 1.73)
+  rasterImage(pic, 1, 1, 2, 2)
   
 }
 # cual es el error de cada punto?
-plot(y = (bots - tops)/(bots[1] - tops[1]) * 100, x = 1:length(files) * 30)
+plot(y = (bots - tops)/(bots[1] - tops[1]) * 100, x = 1:length(files) * 30,
+     xlab = "t(s)", ylab = "% de espuma restante")
 
 #exponential fit
 t <- 30 * 1:length(files)
-plot(t, log(bots - tops))
-aju <- lm(log(bots - tops) ~ t)
+plot(t, log((bots - tops)/(bots[1] - tops[1])))
+aju <- lm(log((bots - tops)/(bots[1] - tops[1])) ~ t - 1)
 abline(aju)
-abline(coefficients(aju) + summary(aju)$coefficients[,2], lty = 3, col = "blue")
-abline(coefficients(aju) - summary(aju)$coefficients[,2], lty = 3, col = "blue")
+abline(a = 0, b = coefficients(aju) + summary(aju)$coefficients[2], lty = 3, col = "blue")
+abline(a = 0, b = coefficients(aju) - summary(aju)$coefficients[2], lty = 3, col = "blue")
 
+
+# h = h_0 e^(-rt)
+# h / h_0 = e^(-rt)
+# ln(h) - ln(h_0) = -rt
+# ln(h) = ln(h_0) - rt -> corte = ln(h_o) y -pendiente = r
+restante = exp(coefficients(aju)[1])
+r = - coefficients(aju)
+r
