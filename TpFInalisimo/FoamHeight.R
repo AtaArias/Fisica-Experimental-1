50841ba786f0abd0cca11b02565febff4e47bd52
AtaArias <atahulpq@gmail.com>
2021-12-16 03:28:39 -0300

    uso de tail para tener una mejor mediana

--- TpFInalisimo/FoamHeight.R
+++ TpFInalisimo/FoamHeight.R
@@ -5,13 +5,13 @@ files <- list.files()
 img <- readPNG(files[9])
 img.filtered <- as.matrix(img[,,1])
 
-# plot(1:2, type = "n")
-# rasterImage(img.filtered, 1.2, 1.27, 1.8, 1.73)
+plot(1:2, type = "n")
+rasterImage(img.filtered, 1.2, 1.27, 1.8, 1.73)
 # 
 # hist(img.filtered)  
 # NCOL(img.filtered)
 
-img.sat <- (img.filtered > 0.7)
+img.sat <- (img.filtered > 0.85)
 storage.mode(img.sat) <-"numeric"
 
 # hist(img.sat)
@@ -33,28 +33,26 @@ for (i in 1:NCOL(img.sat)){
 }
 
 
-
-
-
 tops <- c()
 bots <- c()
-for (i in 1:8) {
+for (i in 1:length(files)) {
   pic <- readPNG(files[i])
   pic <- as.matrix(pic[,,1])
   org <- pic
   
+  #original 0.7
   pic <- (pic > 0.7)
   storage.mode(pic) <- "numeric"
   
   
   h_top <- c()
   h_bot <- c()
-  for (i in 1:NCOL(pic)){
-    columna <- pic[,i]
+  for (j in 1:NCOL(pic)){
+    columna <- pic[,j]
     columna <- columna == 1
     list <- c(FALSE)
-    for (i in 2:length(columna)-1){
-        list <- c(list, (columna[i-1] != columna[i+1]) & columna[i])
+    for (k in 2:length(columna)-1){
+        list <- c(list, (columna[k-1] != columna[k+1]) & columna[k])
     }
     indices <- which(list)
     if (length(indices) > 1){
@@ -62,23 +60,25 @@ for (i in 1:8) {
       h_bot <- c(h_bot, tail(indices,1))
     }
   }
-  tops <- c(tops, mean(h_top))
+  
+  # Que uso mean o median??
+  tops <- c(tops, median(tail(h_top,100)))
   bots <- c(bots, median(h_bot))
   
-  # pic[median(h_top):(median(h_top)+10),] <- 0.5
-  # pic[median(h_bot):(median(h_bot)+10),] <- 0.5
- 
-  org[median(h_top):(median(h_top)+10),] <- 0.5
-  org[median(h_bot):(median(h_bot)+10),] <- 0.5
+  pic[tops[i]:(tops[i]+10),] <- 0.5
+  pic[bots[i]:(bots[i]+10),] <- 0.5
+  
+  org[tops[i]:(tops[i]+10),] <- 0.5
+  org[bots[i]:(bots[i]+10),] <- 0.5
 
   plot(1:2, type = "n")
   rasterImage(org, 1.2, 1.27, 1.8, 1.73)
   
-  # plot(1:2, type= "n")
-  # rasterImage(pic, 1.2, 1.27, 1.8, 1.73)
+  plot(1:2, type= "n")
+  rasterImage(pic, 1.2, 1.27, 1.8, 1.73)
   
 }
-bots
 plot(tops)
-plot(log10(seq(1, 240, 30)),bots-tops)
-plot(tops)
+plot(log(seq(0, 240, 30)),bots-tops)
+plot(bots)
+plot(bots - tops)
