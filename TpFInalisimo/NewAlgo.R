afb56e2d47f5f7abc9b2804c947020395dc6d9be
AtaArias <atahulpq@gmail.com>
2021-12-18 16:56:42 -0300

    Me saco los 23.9 grados por mala calidad de imagen

--- TpFInalisimo/NewAlgo.R
+++ TpFInalisimo/NewAlgo.R
@@ -20,70 +20,91 @@ setwd(root)
 
 temps <- list.files()
 
-setwd(paste(root, temps[10], sep = "/"))
+sens <- c(0.5, 0.55, 0.6, 0.6, 0.5, 0.6, 0.6, 0.53, 0.53)
+t.list <- list()
+total.list <- list()
+for (i in 1:length(temps)){
+  temp <- temps[i]
+  
+  setwd(paste(root, temp, sep = "/"))
+  
+  # Leo la imagen
+  # La paso a blanco y negro
+  # Saco las columnas que suman menos de 10
+  h <- c()
+  t <- c()
+  
+  for (file in list.files()) {
+    img <-file
+    img <- readJPEG(img)
+    img <- as.matrix(img[,,1])
+    
+    # plot(1:2, type= "n")
+    # rasterImage(img, 1, 1, 2, 2)
+    
+    img <- (img > sens[i])
+    storage.mode(img) <- "numeric"
+    
+    # plot(1:2, type= "n")
+    # rasterImage(img, 1, 1, 2, 2)
+    
+    img <- img[,colSums(img) > 10]
+    if (temp == temps[9])
+      img <- img[, (0.1 * ncol(img)):(0.3*ncol(img))]
+    else
+      img <- img[, (0.4 * ncol(img)):(0.75*ncol(img))]
+    
+    # plot(1:2, type= "n")
+    # rasterImage(img, 1, 1, 2, 2)
+    
+    imgEdg <- diff(img)
+    h_top <- c()
+    h_bot <- c()
+    
+    grosor <- c()
+    
+    for (j in 1:ncol(imgEdg)){
+      colum <- imgEdg[,j]
+      
+      debajo <- which(colum == -1)
+      arriba <- which(colum == 1)
+      
+      arriba <- arriba[1:min(length(arriba), length(debajo))]
+      debajo <- debajo[1:min(length(arriba), length(debajo))]
+      
+      height <- debajo - arriba
+      
+      index <- match(max(height), height)
+      
+      grosor <- c(grosor, max(height))
+      
+      h_top <- c(h_top, arriba[index])
+      h_bot <- c(h_bot, debajo[index])
+    }
+    
+    # plot(1:2, type= "n", main = file)
+    # rasterImage(img, 1, 1, 2, 2)
+    # 
+    # abline(h = 2-(median(h_top) / nrow(img)), col = "red", lwd = 5, lty = 3)
+    # abline(h = 2-(median(h_bot, na.rm = T) / nrow(img)), col = "blue", lwd = 5, lty = 3)
 
-# Leo la imagen
-# La paso a blanco y negro
-# Saco las columnas que suman menos de 10
-h <- c()
-t <- c()
-
-for (file in list.files()) {
-  img <-file
-  img <- readJPEG(img)
-  img <- as.matrix(img[,,1])
-  
-  # plot(1:2, type= "n")
-  # rasterImage(img, 1, 1, 2, 2)
-  
-  img <- (img > 0.55)
-  storage.mode(img) <- "numeric"
-  
-  # plot(1:2, type= "n")
-  # rasterImage(img, 1, 1, 2, 2)
-  
-  img <- img[,colSums(img) > 10]
-  img <- img[, (0.4 * ncol(img)):(0.75*ncol(img))]
-  
-  # plot(1:2, type= "n")
-  # rasterImage(img, 1, 1, 2, 2)
-  
-  imgEdg <- diff(img)
-  h_top <- c()
-  h_bot <- c()
-  
-  grosor <- c()
-  for (j in 1:ncol(imgEdg)){
-    colum <- imgEdg[,j]
+    h <- c(h, median(grosor, na.rm = T))
     
-    debajo <- which(colum == -1)
-    arriba <- which(colum == 1)
+    t <- c(t, secs(file))
     
-    arriba <- arriba[1:min(length(arriba), length(debajo))]
-    debajo <- debajo[1:min(length(arriba), length(debajo))]
-    
-    height <- debajo - arriba
-    
-    index <- match(max(height), height)
-    
-    grosor <- c(grosor, max(height))
-    
-    h_top <- c(h_top, arriba[index])
-    h_bot <- c(h_bot, debajo[index])
   }
-  # 
-  # plot(1:2, type= "n")
-  # rasterImage(img, 1, 1, 2, 2)
-  # 
-  # 
-  # abline(h = 2-(median(h_top) / nrow(img)), col = "red", lwd = 5, lty = 3)
-  # abline(h = 2-(median(h_bot) / nrow(img)), col = "blue", lwd = 5, lty = 3)
   
-  h <- c(h, median(grosor))
+  t <- t - t[1]
+  t.list[[i]] <- t
+  total.list[[i]] <- h
   
-  t <- c(t, secs(file))
+  plot(t, h/h[1] * 100, main = temp)
 }
 
-t <- t - t[1]
 
-plot(t, h)
+plot(t.list[[1]], total.list[[1]], pch = 8, col = colores[1], ylim = c(0, 1))
+for (n in 2:length(temps)){
+  # points(x = t.list[[n]], total.list[[n]], pch =  7 + n, col = colores[n])
+  lines(x = t.list[[n]], total.list[[n]], pch =  7 + n, col = colores[n])
+}
+
