045609ef90256c8167daa3a6e145c58dc9ec458c
AtaArias <atahulpq@gmail.com>
2021-12-22 06:56:41 -0300

    Creación de las imagenes

--- TpFInalisimo/ggpotGraphs.R
+++ TpFInalisimo/ggpotGraphs.R
@@ -6,20 +6,21 @@ library(ggplot2)
 
 tidy$Temperatura <- as.numeric(tidy$T)
 
-
+png("TodosExp.png")
 gtutti <- ggplot(data = tidy) +
 geom_point(mapping = aes(x = t, y = h_h0, colour = Temperatura)) + 
 #geom_smooth(aes(x = t, y = h_h0, colour = Temperatura), method = "lm", formula = "y ~ x", show.legend = F) + 
 scale_colour_gradient(low = "blue3", high = "red") + 
 #scale_colour_gradientn(low = "DeepskyBlue4", high = "Red3", ) + 
-scale_y_continuous(trans = "log", breaks = round(seq(1,min((tidy$h_h0)), length.out = 4), 2), labels = round(seq(1,min((tidy$h_h0)), length.out = 4), 2)      )  + 
+# scale_y_continuous(trans = "log", breaks = round(seq(1,min((tidy$h_h0)), length.out = 4), 2), labels = round(seq(1,min((tidy$h_h0)), length.out = 4), 2)      )  + 
 
   
 xlab(TeX("$ t (s) $")) + 
-ylab(TeX(r"($ \\frac{h}{h_0}(adimensional) \[ log \] $)")) 
+ylab(TeX(r"($ \\frac{h}{h_0} \[ log \] $)")) 
 
 show(gtutti)
-
+dev.off()
+setwd("~/Desktop/Experimental1/TpFinal/ggplot graphs")
 for (i in 1:max(tidy$n)){
   datos <- tidy[tidy$n == i,]
   
@@ -29,6 +30,11 @@ for (i in 1:max(tidy$n)){
   
   ecuación = paste(" y = -", round(erre, 4)," x", sep = "")
   ecuación2 = TeX(paste("$$ y = e^{", round(-1 * erre, 4),"x} $$"))
+  
+  name <- paste("AjulinealLog", datos$Temperatura[1], ".png", sep ="_")
+  name2 <- paste("AjulinealExp", datos$Temperatura[1], ".png", sep ="_")
+  
+  png(name)
   
   plot <- ggplot(data = datos) +
     geom_point(mapping = aes(x = t, y = h_h0)) +
@@ -42,7 +48,9 @@ for (i in 1:max(tidy$n)){
     ylab(TeX(r"($ \\frac{h}{h_0} \[ log \] $)")) 
 
   show(plot)
+  dev.off()
   
+  png(name2)
   plot <- ggplot(data = datos) +
     geom_point(mapping = aes(x = t, y = h_h0, colour = Temperatura)) +
     geom_function(fun = function(x) exp(-erre * x)) +
@@ -52,6 +60,7 @@ for (i in 1:max(tidy$n)){
     ylab(TeX(r"($ \\frac{h}{h_0}$)"))
 
   show(plot)
+  dev.off()
 }
 
 tidy[tidy$n == 1,]
